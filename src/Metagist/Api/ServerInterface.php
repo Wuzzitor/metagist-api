fc95433f7b1ed13d680d04e4d59048daad936746
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-22 17:43:22 +0200

    server::pushInfo interface method requires a metainfo object

--- src/Metagist/Api/ServerInterface.php
+++ src/Metagist/Api/ServerInterface.php
@@ -20,10 +20,11 @@ interface ServerInterface
     /**
      * Pushes metainfo to the server.
      * 
-     * @param string $author
-     * @param string $name
-     * @param array  $data
+     * @param string             $author
+     * @param string             $name
+     * @param \Metagist\MetaInfo $info = null
      * @return mixed
+     * @throws \Metagist\Api\Exception
      */
-    public function pushInfo($author, $name, array $data = array());
+    public function pushInfo($author, $name, \Metagist\MetaInfo $info = null);
 }
