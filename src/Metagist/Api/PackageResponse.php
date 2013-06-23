5f1f8568e7d4e1be1de4be268c20a8b86fbf2206
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-23 15:46:27 +0200

    introducing package-service response class

---
+++ src/Metagist/Api/PackageResponse.php
@@ -0,0 +1,60 @@
+<?php
+namespace Metagist\Api;
+
+use Guzzle\Service\Command\ResponseClassInterface;
+
+/**
+ * Server method "package" response class.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class PackageResponse implements ResponseClassInterface
+{
+    /**
+     * package instance.
+     * 
+     * @var \Metagist\Package|null 
+     */
+    private $package;
+    
+    /**
+     * Constructor.
+     * 
+     * @param \Metagist\Package $package
+     */
+    public function __construct(\Metagist\Package $package)
+    {
+        $this->package = $package;
+    }
+    
+    /**
+     * Factory method.
+     * 
+     * @param \Guzzle\Service\Command\OperationCommand $command
+     * @return PackageResponse
+     */
+    public static function fromCommand(\Guzzle\Service\Command\OperationCommand $command)
+    {
+        $json       = $command->getResponse()->json();
+        $identifier = $json['identifier'];
+        $package    = new \Metagist\Package($identifier);
+        $metainfos  = $json['meta_infos'];
+        
+        $collection = new \Doctrine\Common\Collections\ArrayCollection();
+        foreach ($metainfos as $data) {
+            $collection->add(\Metagist\MetaInfo::fromArray($data));
+        }
+        $package->setMetaInfos($collection);
+        return new PackageResponse($package);
+    }
+
+    /**
+     * Returns the package instance.
+     * 
+     * @return \Metagist\Package
+     */
+    public function getPackage()
+    {
+        return $this->package;
+    }
+}
