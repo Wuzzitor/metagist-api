3321961ae66aaaef7e51fbcdf73260adae98fe23
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-15 06:25:05 +0200

    package interface

---
+++ src/Metagist/PackageInterface.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * PackageInterface.php
+ * 
+ * @package metagist-api
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+namespace Metagist;
+
+/**
+ * Interface for packages.
+ * 
+ * @package metagist-api
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+interface PackageInterface
+{
+    /**
+     * Returns the identifier of the package.
+     * 
+     * @return string
+     */
+    public function getIdentifier();
+    
+    /**
+     * Returns the author/owner part of the package identifier.
+     * 
+     * @return boolean
+     */
+    public function getAuthor();
+    
+    /**
+     * Returns the name part of the package identifier.
+     * 
+     * @return string|false
+     */
+    public function getName();
+    
+    /**
+     * Get the description.
+     * 
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * Returns all known versions.
+     * 
+     * @return array
+     */
+    public function getVersions();
+    
+    /**
+     * Returns the type of the package.
+     * 
+     * @return string
+     */
+    public function getType();
+    
+    /**
+     * Returns the associated metainfos.
+     * 
+     * @param string $group
+     * @return \Doctrine\Common\Collections\Collection|null
+     */
+    public function getMetaInfos($group = null);
+    
+    /**
+     * Returns the time of the last update
+     * 
+     * @return \DateTime
+     */
+    public function getTimeUpdated();
+}
